811e45449f1fb2605708fdfdbc27ce515fbfe1d5
Rutvik Rajagopal <rutvik_95@yahoo.co.in>
2014-11-18 01:22:46 +0530

    hostname set

--- drivers/misc/vc04_services/interface/vchiq_arm/vchiq_version.c
+++ drivers/misc/vc04_services/interface/vchiq_arm/vchiq_version.c
@@ -1,8 +1,8 @@
 #include "vchiq_build_info.h"
 #include <linux/broadcom/vc_debug_sym.h>
 
-VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_hostname, "SEV-DEV-SOLUTION" );
-VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_version, "unknown" );
+VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_hostname, "rutvik-MS-7823" );
+VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_version, "abd480dafc177ec6c90ad6dcf829aa9ab2bdfe9e (tainted)" );
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_time,    __TIME__ );
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_date,    __DATE__ );
 
